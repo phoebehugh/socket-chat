8dc658ed628933377ffec274bb8db9486a252f40
Phoebe Hugh <phoebehugh@gmail.com>
2015-05-22 15:59:27 +0100

    Can't have two of the same usernames

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var usernames = [];
+
+http.listen(3000, function(){
+  console.log('Listening on Port 3000');
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,12 +13,20 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  
+  socket.on('new user', function(data, callback) {
+    if (usernames.indexOf(data) != -1){
+      callback(false);
+    } else{
+      callback(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      io.emit('usernames', usernames);
+    }
+  });
+
   socket.on('send message', function(msg){
     console.log('message: ' + msg);
     io.emit('send message', msg);
   });
-});
-
-http.listen(3000, function(){
-  console.log('Listening on Port 3000');
 });
