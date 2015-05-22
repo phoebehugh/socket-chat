067277942cc5a1625676a10c446aefe592e70963
Phoebe Hugh <phoebehugh@gmail.com>
2015-05-22 15:38:59 +0100

    Adds a username page

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('chat message', function(msg){
+  socket.on('send message', function(msg){
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    io.emit('send message', msg);
   });
 });
 
